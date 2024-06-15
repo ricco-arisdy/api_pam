49fa6ccf36630b4de85fdf5e12714bac9352ca62
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-06-15 15:04:34 +0700

    a

--- register.php
+++ register.php
@@ -7,8 +7,16 @@ if ($connect->connect_error) {
     die("Connection failed: " . $connect->connect_error);
 }
 
+// Validasi data POST
+if (!isset($_POST['username'], $_POST['alamat'], $_POST['no_telp'], $_POST['email'], $_POST['password'])) {
+    echo json_encode(array("status" => "error", "message" => "Invalid request"));
+    exit();
+}
+
 // Ambil informasi dari permintaan register
-$username = $_POST['username'];
+$nama = $_POST['username'];
+$alamat = $_POST['alamat'];
+$no_telp = $_POST['no_telp'];
 $email = $_POST['email'];
 $password = $_POST['password']; // Disarankan untuk melakukan hashing pada kata sandi
 
@@ -22,7 +30,8 @@ if ($result->num_rows > 0) {
 } else {
     // Jika email belum digunakan, tambahkan pengguna ke database
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // Hash kata sandi
-    $insertQuery = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashedPassword')";
+    $insertQuery = "INSERT INTO users (username, alamat, no_telp, email, password, created_at) 
+                    VALUES ('$nama', '$alamat', '$no_telp', '$email', '$hashedPassword', NOW())";
     if ($connect->query($insertQuery) === TRUE) {
         // Jika registrasi berhasil, kirimkan respons berhasil
         echo json_encode(array("status" => "success", "message" => "Registrasi berhasil"));
